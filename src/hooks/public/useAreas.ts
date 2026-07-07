d39b56b450989c218cddd07c4838a4d413a05dc6
pratiksha-on-git <pratikshashitole@caryanamindia.in>
2026-07-07 11:52:10 +0530

    changes

--- src/hooks/public/useAreas.ts
+++ src/hooks/public/useAreas.ts
@@ -1,11 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-
+const API_BASE_URL =
+import.meta.env.VITE_API_BASE_URL || "http://localhost:8081";
 export function useAreas() {
   return useQuery<string[]>({
     queryKey: ["all-areas"],
     queryFn: async () => {
-      const { data } = await axios.get("http://localhost:8082/api/pincode/all-areas");
+      const { data } = await axios.get(`${API_BASE_URL}/api/pincode/all-areas`);
       return Array.isArray(data) ? data : [];
     },
     staleTime: 1000 * 60 * 10,
